bc9b4c48681b4be38fc71fed6ffe3da393d8ca70
IrynaBielova <wintergirl@i.ua>
2023-04-03 01:08:51 +0300

    add_changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './js/menu';
-import './js/modal';
+// import './js/menu';
+// import './js/modal';
+// import './js/send-mail'
 import './js/btn-scrollUp';
---
+++ src/js/send-mail.js
@@ -0,0 +1,9 @@
+// function sendMail() {
+//     var link = "mailto:irbielova@gmail.com"
+//              + "?cc=irbielova@gmail.com"
+//              + "&subject=" + encodeURIComponent("This is my subject")
+//              + "&body=" + encodeURIComponent(document.getElementById('myText').value)
+//     ;
+    
+//     window.location.href = link;
+// }
