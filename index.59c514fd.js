6c5ca6b0afb1452dd5b76266d906cabfae04bb9d
IrynaBielova <wintergirl@i.ua>
2023-04-02 22:14:52 +0000

    Deploying to gh-pages from @ IrynaBielova/ruden-ko_online@a9afb077b2265dfcf500e564ee508c6b0feaec99 🚀

---
+++ index.59c514fd.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#arrUp");e.addEventListener("click",(function(e){e.preventDefault(),window.scrollTo({top:0,behavior:"smooth"})})),setInterval((function(){window.scrollY>200?e.style.display="flex":e.style.display="none"}),300);
+//# sourceMappingURL=index.59c514fd.js.map
---
+++ index.9c7c8c86.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#arrUp");e.addEventListener("click",(function(e){e.preventDefault(),window.scrollTo({top:0,behavior:"smooth"})})),setInterval((function(){window.scrollY>200?e.style.display="flex":e.style.display="none"}),300)}();
+//# sourceMappingURL=index.9c7c8c86.js.map
